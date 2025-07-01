3ae40fdf79186bd62855be84ff70d348eacf1e02
12Bits <thelorddosgames@gmail.com>
2025-07-01 16:47:03 -0300

    Update dataloader

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,8 @@ import pandas as pd
 @st.cache_data
 def load_data():
     df = pd.read_csv("data/data_TV.csv") 
-    df["first_air_date"] = pd.to_datetime(df["first_air_date"]).dt.normalize()
+    df["first_air_date"] = pd.to_datetime(df["first_air_date"])
+    df["first_air_date"] = df["first_air_date"].dt.date
     df["year"] = df["first_air_date"].dt.year
     df = df[df["origin_country"] != "Unknown"]
 
