dd63ee8c5e7e527a752a237705d3780c6a1cedb1
12Bits <thelorddosgames@gmail.com>
2025-07-01 16:50:08 -0300

    Update data-loader

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,12 @@
-# data_loader.py
+
 import streamlit as st
 import pandas as pd
 
 @st.cache_data
 def load_data():
     df = pd.read_csv("data/data_TV.csv") 
-    df["first_air_date"] = pd.to_datetime(df["first_air_date"])
-    df["first_air_date"] = df["first_air_date"].dt.date
-    df["year"] = df["first_air_date"].dt.year
+    df["first_air_date"] = pd.to_datetime(df["first_air_date"]).dt.date  # <-- remove hora
+    df["year"] = pd.to_datetime(df["first_air_date"]).dt.year  #  converte de novo para datetime
     df = df[df["origin_country"] != "Unknown"]
-
+    
     return df
